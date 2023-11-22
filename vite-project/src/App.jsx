e777ccee0168a53bb86cd80611eb319130dac861
mitanshu <mitanshubadgujar8@gmail.com>
2023-11-22 20:35:20 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Navbar from "./component/Navbar"
+import Products from "./component/Products"
+import Footer from './component/Footer'
+
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+    <Products/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
